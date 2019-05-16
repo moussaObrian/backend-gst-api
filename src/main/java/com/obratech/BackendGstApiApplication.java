e5ff1285e2c370e924f4d82048b23412e1fdad41
moussaObrian <moussaobrain@gmail.com>
2019-05-16 00:37:21 +0000

    first commit

---
+++ src/main/java/com/obratech/BackendGstApiApplication.java
@@ -0,0 +1,13 @@
+package com.obratech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGstApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGstApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obratech/BackendGstApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obratech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BackendGstApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
